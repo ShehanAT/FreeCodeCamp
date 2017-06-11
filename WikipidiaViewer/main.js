384641cd15085c18953acdbddbe61a66fe42a9ce
Shehan Atukorala <ultimateweapon432@gmail.com>
2017-06-11 12:43:49 -0400

    Create main.js

--- WikipidiaViewer/main.js
+++ WikipidiaViewer/main.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
-  var wikiUrl = "http://en.www.wikipedia.com/w/api.php";
-  wikiURL += "?"
-
+  function wikiViewer(){
+	  $.ajax({
+		  url:"https://en.wikipedia.org/w/api.php?action=opensearch&search=Albert Einstein&limit=400&format=json&callback="
+		, dataType:"jsonp"
+		, success: function(response){
+			console.log(response.search);
+		
+		}
+	  })
+  };
+	wikiViewer();
 });
