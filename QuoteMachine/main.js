f6e26de1132605da13af1e7b3cd901617ac44371
Shehan Atukorala <ultimateweapon432@gmail.com>
2017-06-12 10:38:28 -0400

    Update main.js

--- QuoteMachine/main.js
+++ QuoteMachine/main.js
@@ -1,1 +1,42 @@
+$(document).ready(function(){
+	var quote;
+	var author;
+function getNewQuote(){
+  $.ajax({
+	  
+    url:"http://api.forismatic.com/api/1.0/",
+    jsonp:"jsonp",
+	dataType:"jsonp",
+	data: {
+    method:"getQuote",
+    format:"jsonp",
+	lang:"en",
+    
+    
+  },
+  success: function(response){
+	  quote = response.quoteText;
+	  author = response.quoteAuthor;
+	  $("#quote").text(quote);
+	if (author){
+	$("#author").text("said by:" + author);}
+	else{
+	$("#author").text("- unknown");}}
+  }
+)};
 
+
+  getNewQuote(); 
+  $(".new-quote").on("click", function(event){
+	  event.preventDefault();
+  getNewQuote()
+  });
+    $(".share-quote").on("click", function(event){
+	  window.open("http://twitter.com/intent/tweet?text=" + encodeURIComponent(quote + '--' + author));
+  });
+  
+
+  
+  
+  
+});
