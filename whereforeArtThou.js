543034fd38fcecb1e9325092367214f1055cf17f
Shehan Atukorala <shehanatukorala@gmail.com>
2017-07-11 16:38:31 -0400

    Create whereforeArtThou.js

---
+++ whereforeArtThou.js
@@ -0,0 +1,18 @@
+function whatIsInAName(collection, source) {
+  // What's in a name?
+  var arr = collection.filter(function(item){
+    for (var i in source){
+      if (source[i] != item[i]){
+        return false;
+      }
+    }
+    return true;
+  });
+  // Only change code below this line
+  
+  
+  // Only change code above this line
+  return arr;
+}
+
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
