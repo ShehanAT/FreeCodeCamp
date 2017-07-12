1e88e4a706b5c6ed5f909a44894e53014c78466e
Shehan Atukorala <shehanatukorala@gmail.com>
2017-07-12 11:19:01 -0400

    Create DNAPairing.js

---
+++ DNAPairing.js
@@ -0,0 +1,21 @@
+function PairingDNA(str){
+  str = str.split("");
+  var newArr = [];
+  for (var i in str){
+    if (str[i] == "A"){
+      newArr.push(["A","T"]);
+    }
+    if (str[i] == "T"){
+      newArr.push(["T","A"]);
+    }
+    if (str[i] == "C"){
+      newArr.push(["C","G"]);
+    }
+    if (str[i] == "G"){
+      newArr.push(["G","C"]);
+    }
+  }
+  return newArr;
+
+
+}
