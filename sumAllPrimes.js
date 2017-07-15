85f7bb349f69b9aa666554b02fb686e0bb644820
Shehan Atukorala <shehanatukorala@gmail.com>
2017-07-15 14:53:58 -0400

    Create sumAllPrimes.js

---
+++ sumAllPrimes.js
@@ -0,0 +1,22 @@
+//DO NOT PUT ALL THE LOGIC IN A FOR LOOP 
+function sumPrime(num){
+	var primes = [];
+	for (var i = 2; i <= num; i++){
+		if (isPrime(i)){
+			primes.push(i);
+		}
+	}
+	return primes.reduce(function(a,b){
+		return a + b;
+	});
+}
+	
+	function isPrime(num){
+		for (var j = 2; j < num; j++){
+			if (num % j === 0){
+				return false;
+			}
+		}
+		return true;
+	}
+	
