b9efaa3b408efdf8e4d17a5313e030f50e873ee5
Shehan Atukorala <shehanatukorala@gmail.com>
2017-07-18 16:10:49 -0400

    Update dropIt.js

--- dropIt.js
+++ dropIt.js
@@ -2,7 +2,7 @@
 function dropElements(arr, func) {
   // Drop them elements.
   while(!func(arr[0])){//  ! means false
-    arr.shift();
+    arr.shift();//SHIFT REMOVES THE FIRST ELEMENT OF THE ARRAY 
   }
   return arr;
 }
