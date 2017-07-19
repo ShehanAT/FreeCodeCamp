3a8c2b32840d3f26460ed93cb7bb81fc73f57da2
Shehan Atukorala <shehanatukorala@gmail.com>
2017-07-19 17:02:08 -0400

    Update steamrollArray.js

--- steamrollArray.js
+++ steamrollArray.js
@@ -1,1 +1,13 @@
 
+function steamrollArray(arr){
+	return arr.reduce(function(a, b){
+		if (Array.isArray(b)){
+			return steamrollArray(a.concat(b));
+}
+		else{
+			return a.concat(b);
+}
+},[]);
+}
+steamrollArray([1, [2], [3, [[4]]]]);
+
