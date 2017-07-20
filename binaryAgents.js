7b807f7fb3a2108846dfd01113b08dccef785c01
Shehan Atukorala <shehanatukorala@gmail.com>
2017-07-20 16:06:07 -0400

    Create binaryAgents.js

---
+++ binaryAgents.js
@@ -0,0 +1,9 @@
+function binaryAgent(str) {
+  var placeholder = str.split(" ").map(function(value){
+    
+    return String.fromCharCode(parseInt(value,2));// CONVERT BINARY STRINGS TO ENGLISH,
+  }).join("");
+  return placeholder;
+}
+
+binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
