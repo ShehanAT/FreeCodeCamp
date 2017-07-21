3a0802363341176c0b17d63197a574ab6bed96ec
Shehan Atukorala <shehanatukorala@gmail.com>
2017-07-21 12:00:02 -0400

    Update everythingBeTrue.js

--- everythingBeTrue.js
+++ everythingBeTrue.js
@@ -11,3 +11,12 @@ function truthCheck(collection, pre){
   }
 }
 truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
+
+
+// STEPHAN MAYEUX SOLUTION 
+function truthCheck(collection, pre){
+  return collection.every(function(obj){
+    return obj[pre];
+  })
+}
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex")
