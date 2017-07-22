8d3b1a2bd54efa76be0f2ca2dade3d0fce96c769
Shehan Atukorala <shehanatukorala@gmail.com>
2017-07-22 13:07:44 -0400

    Update argumentOptional.js

--- argumentOptional.js
+++ argumentOptional.js
@@ -3,4 +3,28 @@ function checkIfNumber(num){
     return typeof num === "number" ? num : undefined;
 
 }
+    var a = checkIfNumber(arguments[0]);
+    var b = checkIfNumber(arguments[1]);
+    
+    if (arguments.length > 1){
+        return a && b ? a + b : undefined; 
+    }
+    else{
+        if (a){
+            return checkIfNumber(y){
+                if checkIfNumber(y){
+                    return a + y;
+                }
+                else{
+                    return undefined;
+                }
+            }
+        }
+       else{
+           return undefined;
+       }
+        }
+    }
+    
+    }
 }
