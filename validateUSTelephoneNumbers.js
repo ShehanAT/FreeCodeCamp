69bbf98bd51cffdf89354edd1015585863cf9274
Shehan Atukorala <shehanatukorala@gmail.com>
2017-07-24 12:21:26 -0400

    Update validateUSTelephoneNumbers.js

--- validateUSTelephoneNumbers.js
+++ validateUSTelephoneNumbers.js
@@ -1,1 +1,10 @@
 
+function telephoneCheck(str) {
+  // Good luck!
+  var check = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/gm;
+  return check.test(str);
+}
+
+
+
+telephoneCheck("555-555-5555");
