1d84855f3221ad80846e5b0f67abffcaf54a2622
Shehan Atukorala <shehanatukorala@gmail.com>
2017-07-25 20:52:24 -0400

    Update accessingNestedObjects.js

--- Basic_Javascript/accessingNestedObjects.js
+++ Basic_Javascript/accessingNestedObjects.js
@@ -1,1 +1,20 @@
 
+// Setup
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+// Only change code below this line
+
+var gloveBoxContents = "maps"; // Change this line
+gloveBoxContents = myStorage.car.inside["glove box"];
+
+
